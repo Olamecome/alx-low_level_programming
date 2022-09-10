221e6145364523088d624f6aca833451bd0bb956
Olamecome <olamecome@ymail.com>
2022-09-10 07:47:30 -0700

    Betty dey worry

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,18 @@
 /*
- *
+ *Initial description: This code is to print an output
  */
 #include <stdio.h>
 
 /**
  * Main function print 
- * \"Programming is like building a multilingual puzzle
+ * Programming is like building a multilingual puzzle
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+	/**
+	 * Returns 0
+	 */
 	return (0);
 }
