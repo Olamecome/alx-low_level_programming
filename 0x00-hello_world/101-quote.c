b07901bcd706dec83e3fc98741895e59e62041e9
Olamecome <olamecome@ymail.com>
2022-09-10 08:59:07 -0700

    datatype size

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - main function to print
+ * description - Function print string with special characters
+ * Return: return value is 1
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
