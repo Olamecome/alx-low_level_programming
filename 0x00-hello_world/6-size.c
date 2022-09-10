39716d7a9a6ca56457d76bfa875268f8dc007995
Olamecome <olamecome@ymail.com>
2022-09-10 08:43:00 -0700

    datatype size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,7 +17,7 @@ int main(void)
 	 * evaluates the sizes of these data types
 	 */
 	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-	printf("Size of an int: %zu bytes(s)\n", sizeof(intType));
+	printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(longInt));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongInt));
 	printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
