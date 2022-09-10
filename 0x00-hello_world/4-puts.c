fbebedabe5db022e5c8728d6ff1142e33b663428
Olamecome <olamecome@ymail.com>
2022-09-10 07:34:57 -0700

    Betty dey worry

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,13 +2,15 @@
  *C coding using betty format ALX
  */
 #include <stdio.h>
+
 /*
  *Function descritopn as Betty wants
  */
-
 int main(void)
 {
-	/*This prints without using the print f*/
+	/*This prints without using
+	 *  the print f
+	 *  */
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
