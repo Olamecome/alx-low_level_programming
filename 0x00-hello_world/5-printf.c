9dcad8ab8806d5793e805906732caf02f1ef1e96
Olamecome <olamecome@ymail.com>
2022-09-10 08:19:23 -0700

    Betty dey worry

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - This is the main function that prints with printf
+ * description - This returns a 0 value
+ * Return: Zero
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
