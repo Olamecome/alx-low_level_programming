37df2713ae86c86afd20b8f3f35b258d460a922c
Olamecome <olamecome@ymail.com>
2022-09-11 07:26:09 -0700

    More control flows

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - FUnction to print the alphabets in lower case
+ * Return: Function returns 0 - Success
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar("\n");
+}
