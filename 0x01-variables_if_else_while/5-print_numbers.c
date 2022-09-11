eac5821f795723cc8bf3ae6b95bdcd7bcf069855
Olamecome <olamecome@ymail.com>
2022-09-11 07:42:45 -0700

    More control flows

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Function to print the digits 0-9
+ * Return: Function returns 0 - Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf(i);
+	}
+	printf('\n');
+
+	return (0);
+}
