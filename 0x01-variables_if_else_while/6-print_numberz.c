c6369413222aa7137d2a05a104a3598f4cef8c81
Olamecome <olamecome@ymail.com>
2022-09-11 07:50:33 -0700

    More control flows

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - FUnction to print numbers in string
+ * Return: Function returns 0 - Success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
