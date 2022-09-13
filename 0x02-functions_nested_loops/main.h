5c49db8481f24b445cd7cc0290fdd65e34fad997
Olamecome <olamecome@ymail.com>
2022-09-13 13:01:54 -0700

    Creating custom header file with custom print function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,11 @@
  * _putchar - function to print characters
  * Return: Return 1 if success, -1 if failed
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+/**
+ * int _putchar(char c)
+ * {
+ * 	return (write(1, &c, 1));
+ * }
+ */
+
+int _putchar(char c);
