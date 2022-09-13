33ee68a3da4affd8737b4a68c1519b873e0e8156
Olamecome <olamecome@ymail.com>
2022-09-13 15:04:05 -0700

    Creating custom header file with custom print function

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * _islower - Check if character is lowercase
+ * @c: character to be evaluated
+ * Return: returns 1 if character is lowercase, 0 if it isnt
+ */
+int _islower(int c)
+{
+	value = islower(c) == 0 ? 0 : 1;
+	return (value);
+}
