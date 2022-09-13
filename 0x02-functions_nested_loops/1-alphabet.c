31937d8e368ec446ed97364f40fdc407de592f94
Olamecome <olamecome@ymail.com>
2022-09-13 13:50:21 -0700

    Creating custom header file with custom print function

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,4 @@
-#include <stdio.h>
-
-/**
- * print_alphabet - function to print alphabets in lower letter
- * Return: returns 0 meaning success
- */
-void print_alphabet(void)
-{
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-		putchar(c);
-	putchar('\n');
-}
+#include "main.h"
 
 /**
  * main - check the code
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,14 +1,30 @@
 #include <unistd.h>
 
+int _putchar(char c);
+void print_alphabet(void);
+
 /**
  * _putchar - function to print characters
  * Return: Return 1 if success, -1 if failed
  */
-/**
- * int _putchar(char c)
- * {
- * 	return (write(1, &c, 1));
- * }
- */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
-int _putchar(char c);
+/**
+ * print_alphabet - function to print lowercase alphabets
+ * Return: Zero for success
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+
+	return (0);
+}
