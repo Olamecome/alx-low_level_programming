b47d4dac6ec5e1fb4e510a2b5579e1107d0c1b9c
Olamecome <olamecome@ymail.com>
2022-09-13 13:52:39 -0700

    Creating custom header file with custom print function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,10 +7,13 @@ void print_alphabet(void);
  * _putchar - function to print characters
  * Return: Return 1 if success, -1 if failed
  */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
+
+/**
+ * int _putchar(char c)
+ * {
+ * 	return (write(1, &c, 1));
+ * }
+ */
 
 /**
  * print_alphabet - function to print lowercase alphabets
