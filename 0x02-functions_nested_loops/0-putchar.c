b81f957b08d7fa6b267c2a574d48ad4ff4d052ec
Olamecome <olamecome@ymail.com>
2022-09-13 12:26:54 -0700

    custom print function

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,14 @@
-#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - function to putchar
+ * @c: value to be printed
+ * Return: on success, 1. on error, -1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 
 /**
  * main - function to print out _putchar
@@ -6,6 +16,14 @@
  */
 int main(void)
 {
-	printf("_putchar\n");
+	char string[] = "_putchar";
+	int i;
+	int len;
+
+	len = sizeof(string) / sizeof(char);
+
+	for (i = 0; i < len; i++)
+		_putchar(string[i]);
+	_putchar('\n');
 	return (0);
 }
