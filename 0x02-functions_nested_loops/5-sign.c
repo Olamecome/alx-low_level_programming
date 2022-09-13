e0a4d5d2b7c8f2bcd68236cef3ae32c9820c1cc6
Olamecome <olamecome@ymail.com>
2022-09-13 15:38:10 -0700

    Creating custom header file with custom print function

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - Print the sign of a number
+ * @n: Number sign is to be checked for
+ * Return: 1 if sign is positive,
+ * 0 if number is zero
+ * -1 if number is negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
