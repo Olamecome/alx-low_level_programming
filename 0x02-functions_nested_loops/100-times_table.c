c40c27a242fdd2822601e437c4cedd48521a74d6
Olamide Kalesanwo <34948273+Olamecome@users.noreply.github.com>
2022-09-14 02:41:39 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,35 +9,35 @@ void print_times_table(int n)
 {
   int a, b, c;
   if (n >= 0 && n <= 15)
+  {
+    for (a = 0; a <= n; a++)
     {
-      for (a = 0; a <= n; a++)
+      putchar(48);
+      for (b = 1; b <= n; b++)
       {
-        putchar(48);
-        for (b = 1; b <= n; b++)
+        c = a * b;
+        putchar(44);
+        putchar(32);
+        if (c <= 9)
         {
-          c = a * b;
-          putchar(44);
           putchar(32);
-          if (c <= 9)
-          {
-            putchar(32);
-            putchar(32);
-            putchar(c + 48);
-          }
-          else if (c <= 99)
-          {
-            putchar(32);
-            putchar((c / 10) + 48);
-            putchar((c % 10) + 48);
-          }
-          else
-          {
-            putchar(((c / 100) % 10) + 48);
-            putchar(((c / 10) % 10) + 48);
-            putchar((c % 10) + 48);
-          }
+          putchar(32);
+          putchar(c + 48);
         }
-        putchar('\n');
+        else if (c <= 99)
+        {
+          putchar(32);
+          putchar((c / 10) + 48);
+          putchar((c % 10) + 48);
+        }
+        else
+        {
+          putchar(((c / 100) % 10) + 48);
+          putchar(((c / 10) % 10) + 48);
+          putchar((c % 10) + 48);
+        }
       }
+      putchar('\n');
+    }
   }
 }
