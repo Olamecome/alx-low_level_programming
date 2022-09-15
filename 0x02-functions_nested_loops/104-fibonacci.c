b2ed18861ff95c4c95b9bb82b10fcd6a20751e1c
Olamecome <olamecome@ymail.com>
2022-09-14 18:10:53 -0700

    Nested loop

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Print Fibonacci series
+ * Return: Zero
+ */
+int main(void)
+{
+	long t1 = 1;
+	long t2 = 2;
+	long next = t1 + t2;
+	int num = 98 - 2;/* to remove first two values from the total loop*/
+
+	printf("%ld, %ld, ", t1, t2);
+
+	while (num > 0)
+	{
+		printf("%ld", next);
+		/* prints comma and space between items excet the last one*/
+		if (num > 1)
+			printf(", ");
+		t1 = t2;
+		t2 = next;
+		next = t1 + t2;
+		num--;
+	}
+	printf("\n");
+
+	return (0);
+}
