13ad0cd7d4c7391e494bf2d630f4c9c92c100375
Olamecome <olamecome@ymail.com>
2022-09-14 18:47:51 -0700

    Nested loop

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 
+
+
+void fibonacci(void);
+
 /**
  * main - chack code
  * Return: Zero
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,4 +13,4 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
-void fibonacci(void)
+void fibonacci(void);
