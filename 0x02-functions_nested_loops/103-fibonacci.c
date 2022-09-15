5edefb0384a541ea132f81caa29995a1eb9892e3
Olamecome <olamecome@ymail.com>
2022-09-14 18:01:21 -0700

    Nested loop

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Print Fibonacci series
+ * Return: Zero
+ */
+int main(void)
+{
+	long t1 = 1;
+	long t2 = 2;
+	long next = t1 + t2;
+	long sum;
+
+	while (next <= 4000000)
+	{
+		if (next % 2 == 0)
+			sum += next;
+
+		t1 = t2;
+		t2 = next;
+		next = t1 + t2;
+	}
+	printf("%ld\n", sum);
+
+	return (0);
+}
