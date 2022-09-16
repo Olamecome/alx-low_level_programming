71b8e3e992aa1e22379e75aa5cb5b48faf8c4e64
Olamecome <olamecome@ymail.com>
2022-09-16 16:03:00 -0700

    More loops and functions

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Function to print _ number of times passed
+ * @n: integer value for number of times _ should be printed
+ * if @n is 0 or less, new line should be printed
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
