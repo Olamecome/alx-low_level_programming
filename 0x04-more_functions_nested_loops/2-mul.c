89581384e142e31c880d882a067c121805af685c
Olamecome <olamecome@ymail.com>
2022-09-16 03:27:51 -0700

    More Nested lops and functions

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * mull - multiplies two numbers
+ * mul - multiplies two numbers
  * Description: Multiplys two integers and returns the result
  * @a: The first number to be multiplied
  * @b: The second number to be multiplied
