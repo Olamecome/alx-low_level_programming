d03740400abb3fd1d13a609ad70042d2c2afdcf6
Olamecome <olamecome@ymail.com>
2022-09-16 16:17:22 -0700

    More loops and functions

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Function to print \ across the screen
+ * @n: integer value for number of times \ should be printed
+ * if @n is 0 or less, new line should be printed
+ */
+void print_line(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
