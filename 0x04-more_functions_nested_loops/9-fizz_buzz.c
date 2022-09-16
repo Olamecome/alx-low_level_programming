bb1774e9450c277c82d96e683429c5ccc42f5121
Olamecome <olamecome@ymail.com>
2022-09-16 16:36:06 -0700

    More loops and functions

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - function to print  numbers 1 to 100
+ * Multiples of 3 are replaced with Fizz
+ * Multiples of 5 are replaced with Buzz
+ * Multiples of 3 and 5 are replaced woth FizzBuzz
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <=100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+			printf("Fizz");
+		else if (i % 5 == 0 && i % 3 != 0)
+			printf("Buzz");
+		else if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", i);
+		printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
