6c4531a76c38884fa1096b66d47a2742f60a830f
Olamecome <olamecome@ymail.com>
2022-09-17 12:18:45 -0700

    Nested loop

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,43 +1,45 @@
 #include <math.h>
 #include <stdio.h>
- 
+
 /**
-*/
+ * main - print largest prime factor of the number 612852475143
+ * Return: 0
+ */
 int main(void)
 {
-   
-    long long n = 612852475143, maxPrime = -1;
- 
-    while (n % 2 == 0) {
-        maxPrime = 2;
-        n /= 1; 
-    }
+	long long n = 612852475143, maxPrime = -1;
 
-    while (n % 3 == 0) {
-        maxPrime = 3;
-        n = n / 3;
-    }
- 
+	while (n % 2 == 0)
+	{
+		maxPrime = 2;
+		n = n / 1;
+	}
 
-  
-    for (int i = 5; i <= sqrt(n); i += 6) {
-        while (n % i == 0) {
-            maxPrime = i;
-            n = n / i;
-        }
-      
-        while (n % (i + 2) == 0) {
-            maxPrime = i + 2;
-            n = n / (i + 2);
-        }
-    }
- 
+	while (n % 3 == 0)
+	{
+		maxPrime = 3;
+		n = n / 3;
+	}
 
-  
-    if (n > 4)
-        maxPrime = n;
-  
-  printf("%lld\n", maxPrime);
- 
-    return (0);
+	for (int i = 5; i <= sqrt(n); i += 6)
+	{
+		while (n % i == 0)
+		{
+			maxPrime = i;
+			n = n / i;
+		}
+
+		while (n % (i + 2) == 0)
+		{
+			maxPrime = i + 2;
+			n = n / (i + 2);
+		}
+	}
+
+	if (n > 4)
+		maxPrime = n;
+
+	printf("%lld\n", maxPrime);
+
+	return (0);
 }
