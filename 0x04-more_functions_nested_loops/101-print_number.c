c772417c80a7e8c4d27168c650e81dc268654e2e
Olamecome <olamecome@ymail.com>
2022-09-17 11:44:25 -0700

    Nested loop

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,6 +2,7 @@
 
 /**
  * print_number - Prints numbers using _putchar
+ * @n: Number to be printed
  * Return: 0
  */
 void print_number(int n)
@@ -15,6 +16,9 @@ void print_number(int n)
 
 	if (n / 10)
 		print_number(n / 10);
+
+	if (n == 0)
+		_putchar('0');
 
 	_putchar(n % 10 + '0');
 }
