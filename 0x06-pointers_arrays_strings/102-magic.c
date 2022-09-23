93a54d33c93a9a1197fcd6cb1c5afb05d65abef7
Olamecome <olamecome@ymail.com>
2022-09-23 02:12:56 -0700

    Pointers

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,19 +6,13 @@
  */
 
 int main(void)
-
 {
-
   int n;
-
   int a[5];
-
   int *p;
 
 
-
   a[2] = 1024;
-
   p = &n;
 
   /*
--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * print_number - printing number with puts
  *
@@ -9,57 +7,33 @@
  */
 
 void print_number(int n)
-
 {
 
 	/*Declaring variables*/
-
 	int count = 0, pow = 1;
-
 	unsigned int num = n;
 
-
-
-
-
 	if (n < 0) /*Evaluate this condition*/
-
 	{
-
 		_putchar('-');
-
 		num = -n;
-
 	}
 
 	while (n != 0)
-
 	{
-
 		n /= 10;
-
 		count++;
-
 	}
 
 	while (count > 1)
-
 	{
-
 		pow *= 10;
-
 		count--;
-
 	}
 
 	while (pow >= 1)
-
 	{
-
 		_putchar(num / pow % 10 + '0');
-
 		pow /= 10;
-
 	}
-
 }
