05f06306a8114dc96ab710663fb11a1168f50174
Olamecome <olamecome@ymail.com>
2022-09-23 01:36:35 -0700

    Pointers

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncat - function to concatenate pointer strings
+ * @dest: Destination string
+ * @src: Second string to be appended to dest
+ * @n: size of src
+ * Return: destination after appending src
+ */
+char *_strcat(char *dest, char *src, int n)
+{
+	int i = 0, j;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	for (j = 0; src[j] != '\0' && j < n; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+
+	dest[i] = '\0';
+
+	return (dest);
+}
