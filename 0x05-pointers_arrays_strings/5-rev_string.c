aa8e1d68ceb43f403aea3b8fb096303308ca4df2
Olamecome <olamecome@ymail.com>
2022-09-23 20:53:09 -0700

    Pointers and arrays

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,14 +8,17 @@
 void rev_string(char *s)
 {
 	int l, i;
-	char ch;
+	char *ch;
+
+	ch = s;
 
 	for (l = 0; s[l] != '\0'; ++l)
 
-	for (i = 0; i < l / 2; ++i)
+	for (i = 0; i < l; ++i)
 	{
-		ch = s[i];
+		/*ch = s[i];
 		s[i] = s[l - 1 - i];
-		s[l - 1 - i] = ch;
+		s[l - 1 - i] = ch;*/
+		s[i] = ch [l - i];
 	}
 }
