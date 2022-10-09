83823f1993186f3533f4972f8d76b41f5b1f49df
Olamide Kalesanwo <34948273+Olamecome@users.noreply.github.com>
2022-10-09 20:19:07 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,11 +8,9 @@
 * @argv: pointer to array of arguments
 * Return: 0 on success
 */
-
 int main(int argc, char *argv[])
 {
-if (argc > 0)
-printf("%s\n", argv[0]);
+  printf("%s\n", argv[0]);
 
-return (0);
+  return (0);
 }
