6bc1b10a5332cd0c1593685af9565efccf214ac7
Olamide Kalesanwo <34948273+Olamecome@users.noreply.github.com>
2022-10-09 20:22:34 +0100

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,10 +7,8 @@
 * Return: 0 on success
 */
 
-int main(int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[])
 {
-if (argc > 0)
-printf("%d\n", argc - 1);
-
-return (0);
+  printf("%d\n", argc);
+  return (0);
 }
