5220b2522e2f1a370448dc945cc355d024587c09
Olamecome <olamecome@ymail.com>
2022-10-11 18:12:35 -0700

    Struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,9 +12,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
-	new_dog->name = name;
+	new_dog->name = *name;
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = *owner;
 
 	new_dog->name == NULL || new_dog->owner == NULL ? return (NULL) : return (new_dog);
 }
